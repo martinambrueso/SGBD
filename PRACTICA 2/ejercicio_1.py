47d217363dde5c83b2729a0241e601bfea9cbab1
MA_E90832 <m.ambrueso@redlink.com.ar>
2021-04-30 16:37:55 -0300

    Se suben correcciones del 1 y el 3

--- PRACTICA 2/ejercicio_1.py
+++ PRACTICA 2/ejercicio_1.py
@@ -2,6 +2,7 @@ import re
 import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
+import math
 
 def generarGráficoDeBarrasPorAmbientes(departamentes_capital_df):
 	filtrado_outliers = departamentes_capital_df[(departamentes_capital_df['rooms'] > 0) & (departamentes_capital_df['rooms'] < 9)]
@@ -9,6 +10,7 @@ def generarGráficoDeBarrasPorAmbientes(departamentes_capital_df):
 	#agrupado_por_ambiente = filtrado_outliers_final.groupby(['rooms']).count()
 	
 	agrupado_por_ambiente = filtrado_outliers.value_counts(['rooms']).reset_index(name='counts')
+	print(agrupado_por_ambiente)
 	
 	#print(agrupado_por_ambiente)
 	
@@ -25,12 +27,11 @@ def main():
 	df = pd.read_csv('properati-AR-2018-02-01-properties-sell.csv')
 	
 	departamentes_capital_df = df[(df['state_name'] == 'Capital Federal') & (df['property_type'] == 'apartment')]
-	print(departamentes_capital_df)
 	dos_ambientes_df = departamentes_capital_df[departamentes_capital_df['rooms'] == 2]
 	precio_total = dos_ambientes_df['price_aprox_local_currency'].sum()
 
 	valorMedio = precio_total/departamentes_capital_df.size
-	print("Valor medio de deptos 2 ambientes: {}".format(valorMedio))
+	print("Valor medio de deptos 2 ambientes: {:.2f}".format(valorMedio))
 
 	generarGráficoDeBarrasPorAmbientes(departamentes_capital_df)
 	generarGráficoDeBarrasHorizontal(dos_ambientes_df)
--- PRACTICA 2/ejercicio_3.py
+++ PRACTICA 2/ejercicio_3.py
@@ -12,13 +12,21 @@ def main():
 	latitud_sid_campeador = 34.60689190269493
 	longitud_sid_campeador = 58.445861073121215
 
-	ciudades_mayor_poblacion_df = df[(df['place_name'] == 'Buenos Aires') |
-		 (df['place_name'] == 'Córdoba') |
-		 (df['place_name'] == 'Rosario') |
-		 (df['place_name'] == 'Mar del Plata') |
-		 (df['place_name'] == 'La Plata') &
-		 (df['property_type'] == 'apartment') &
-		 (df['rooms'] == 3)]
+	#grouped_df = df[df['place_name'] == 'Bs.As. G.B.A. Zona Oeste']
+	#print(grouped_df)
+
+	ciudades_mayor_poblacion_df = df[
+		(df['place_name'] == 'Bs.As. G.B.A. Zona Oeste') |
+		(df['place_name'] == 'Bs.As. G.B.A. Zona Norte') |
+		(df['place_name'] == 'Bs.As. G.B.A. Zona Sur') |
+		(df['place_name'] == 'Bs.As. G.B.A. Zona Este') |
+		(df['place_name'] == 'Córdoba') |
+		(df['place_name'] == 'Rosario') |
+		(df['place_name'] == 'Mar del Plata') |
+		(df['place_name'] == 'La Plata') &
+		(df['property_type'] == 'apartment') &
+		(df['rooms'] == 3)
+	]
 
 	ciudades_mayor_poblacion_df.boxplot(column=['price'], by = 'place_name')
 	plt.show()
