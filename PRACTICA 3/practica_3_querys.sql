58c1b0c12c066242c84bfd9824d2ab3fdbfcc140
MAURO MARTIN BONANNO <mauro.bonanno@mercadolibre.com>
2021-05-12 19:57:50 -0300

    comentarios para arreglar

--- PRACTICA 3/practica_3_querys.sql
+++ PRACTICA 3/practica_3_querys.sql
@@ -11,23 +11,27 @@ select name from country where continent = 'South America' and population > 1500
 select name, gnp from country order by gnp desc limit 10;
 
 -- Forma de gobierno y cantidad de paises con dicha forma de gobierno ordenados por cantidad de modo descentente
-select governmentform, count(name) as cantidad_de_paises from country group by governmentform order by cantidad_de_paises desc;
+select governmentform, count(name) as cantidad_de_paises from country 
+group by governmentform order by cantidad_de_paises desc;
 
 -- Los nombres de los continentes con sus respectivas superficies ordenados de forma descendentes por superficie
 select continent, sum(surfacearea) as superficie from country group by continent order by superficie desc;
 
 -- Los continentes y la cantidad de paises que los componen de aquellos continentes con mas de 15 paıses
-select continent, count(name) as cantidad_de_paises from country group by continent HAVING count(name) > 15 order by cantidad_de_paises desc;
+select continent, count(name) as cantidad_de_paises from country 
+group by continent HAVING count(name) > 15 order by cantidad_de_paises desc;
 
 -- Idem punto anterior pero que los pa ́ıses que se tengan en cuenta tengan una poblacion de mas de 20 millones de personas
-select continent, count(name) as cantidad_de_paises from country where population > 20000000 group by continent HAVING count(name) > 15 order by cantidad_de_paises desc;
+select continent, count(name) as cantidad_de_paises 
+from country where population > 20000000 group by continent HAVING count(name) > 15 order by cantidad_de_paises desc;
 
 
 -- Subqueries
 
 -- Nombre del pais y expectativa de vida de los paises con mayor y menor expectativa de vida
 select name, lifeexpectancy from country 
-where lifeexpectancy = (select min(lifeexpectancy) from country) or lifeexpectancy = (select max(lifeexpectancy) from country);
+where lifeexpectancy = (select min(lifeexpectancy) from country) 
+or lifeexpectancy = (select max(lifeexpectancy) from country);
 
 -- Nombre de los paises y año de independencia que pertenecen al coninente del paìs que se independizo hace más tiempo
 select name, indepyear from country
@@ -35,7 +39,7 @@ where continent = (select continent from country where indepyear = (select min(i
 
 
 -- Nomber de los continentes que no pertenecen al conjunto de los continentes más pobres
-
+-- utilizando gnp, utilizando alguna condicion
 
 
 -- Joins
@@ -47,14 +51,16 @@ where cy.continent = 'Oceania';
 -- Los Paises y la cantidad de lenguas de los que se habla más de una lengua. Ordenado de forma descendente
 select cy.name, count(cl.language) as cantidad_de_lenguas from country cy
 inner join countrylanguage cl on cl.countrycode = cy.code
-where cy.continent = 'Oceania'
+-- where cy.continent = 'Oceania'
 group by cy.name
-having count(Distinct(cl.language)) > 1
+having count(cl.language) > 1
 order by cantidad_de_lenguas desc;
 
-
 -- Lenguas que se hablan en el continente más pobre, sin contar antarctica
-select continent, sum(gnp) as total_gnp from country where continent != 'Antarctica' group by continent order by total_gnp asc limit 1;
+-- avg de gnp 
+select continent, sum(gnp) as total_gnp from country 
+where continent != 'Antarctica' 
+group by continent order by total_gnp asc limit 1;
 
 select cl.language from country cy
 inner join countrylanguage cl on cl.countrycode = cy.code
@@ -71,9 +77,8 @@ select cy.name, sum(ct.population) as poblacion,
 (sum(ct.population) * 100) / sum(cy.population) as porcentaje
 from country cy
 inner join city ct on ct.countrycode = cy.code
+where cy.name = 'China'
 group by cy.name order by porcentaje desc;
-
-
 
 -- Analizar los scripts de creacion de las tres tablas (country, city, countrylanguage) y responder a las siguientes preguntas:
 -- Cual es la clave primaria (primary key) de cada tabla?
@@ -111,12 +116,12 @@ TABLESPACE pg_default;
 ALTER TABLE public.stats
     OWNER to postgres;
 
+select * from stats;
 
-
+-- Cambiar por 1 insert y 1 update
 INSERT INTO public.stats
 select cy.code, count(cl.language), sum(ct.population) from country cy
 inner join countrylanguage cl on cl.countrycode = cy.code
 inner join city ct on ct.countrycode = cy.code
 group by cy.code;
 
-select * from stats;
