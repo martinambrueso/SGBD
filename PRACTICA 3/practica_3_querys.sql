2b30e0f6d8b4a4aad0d3cc4a378952d13f204ff1
MA_E90832 <m.ambrueso@redlink.com.ar>
2021-05-12 17:58:28 -0300

    Se agrega punto 4 practica 3

--- PRACTICA 3/practica_3_querys.sql
+++ PRACTICA 3/practica_3_querys.sql
@@ -75,4 +75,46 @@ group by cy.name order by porcentaje desc;
 
 
 
+Analizar los scripts de creacion de las tres tablas (country, city, countrylanguage) y responder a las siguientes preguntas:
+Cual es la clave primaria (primary key) de cada tabla?
+country - code
+countrylanguage - countrycode, language
+city - id
 
+Cuales son las claves foraneas (foreign key) de cada tabla?
+country - capital
+countrylanguage - countrycode
+city -  no tiene
+
+A tu criterio, falta alguna clave foranea?
+Como criterio tomo las definiciones del constraint sql en la definicion ddl. No falta ninguna clave, ya que las relaciones  se establecen solo 
+desde countrylanguage > country, country > city, no se detalla acerca de mas relaciones (countrylanguage > city).
+
+-- Table: public.stats
+
+-- DROP TABLE public.stats;
+
+CREATE TABLE public.stats
+(
+    countrycode character(4) COLLATE pg_catalog."default" NOT NULL,
+    cant_lenguas numeric NOT NULL,
+    pop_urbana numeric NOT NULL,
+    CONSTRAINT stats_pkey PRIMARY KEY (countrycode),
+    CONSTRAINT countrycode FOREIGN KEY (countrycode)
+        REFERENCES public.country (code) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.stats
+    OWNER to postgres;
+
+
+
+INSERT INTO tblA
+select cy.code, count(cl.language), sum(cy.population) from country cy
+inner join countrylanguage cl on cl.countrycode = cy.code
+where cy.continent = 'Oceania'
+group by cy.code;
