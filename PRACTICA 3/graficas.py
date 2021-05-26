b0e2b0e2f86b7b6664c1d68f07719945fbeaacc5
MAURO MARTIN BONANNO <mauro.bonanno@mercadolibre.com>
2021-05-26 19:59:25 -0300

    Arreglo script

--- PRACTICA 3/graficas.py
+++ PRACTICA 3/graficas.py
@@ -4,43 +4,55 @@ from shapely.geometry import Point
 from geopandas import GeoSeries, GeoDataFrame
 import csv
 import psycopg2
+import math
 
 hostname = 'localhost'
 username = 'postgres'
-password = 'docker'
-database = 'sgbd'
+#password = 'docker'
+password = 'admin'
+#database = 'sgbd'
+database = 'world'
 
-queryEJ1 = 'SELECT name, gnp FROM public.country ORDER BY name'
-queryEJ2 = 'SELECT name, population FROM public.country ORDER BY name'
-queryEJ3 = 'SELECT pais, count(entidad) FROM public.sitio GROUP BY pais'
+queryEJ1 = 'SELECT code, population FROM public.country ORDER BY code'
+queryEJ2 = 'SELECT code, gnp FROM public.country ORDER BY code'
+queryEJ3 = 'SELECT countrycode, count(entidad) FROM public.sitio GROUP BY countrycode ORDER BY countrycode'
 
-def getData(conexion):
+def getData(conexion, query):
     dictionary = {}
 
     cur = conexion.cursor()
-    cur.execute(queryEJ3)
+    cur.execute(query)
     
     for elem in cur.fetchall():
-        dictionary[elem[0]] = elem[1]
-
+        dictionary[elem[0].strip()] = elem[1]
+        
     return dictionary
 
 
-
-def main():
-    world = GeoDataFrame.from_file('ne_10m_admin_0_countries.shp').sort_values(by='NAME').set_index('NAME')
-    listDf = world.index.tolist()
-
+def processMap(world, listDf, query, alphaValue):
     conexion = psycopg2.connect( host=hostname, user=username, password=password, database=database )
-    resultDB = getData(conexion)
+    resultDB = getData(conexion, query)
     
     for elem in listDf:
         if elem in resultDB:
-            world.at[elem, 'distribution'] = resultDB[elem]
+            if resultDB[elem] > 0:
+                world.at[elem, 'distribution'] = math.log2(resultDB[elem])
+            else:
+                world.at[elem, 'distribution'] = resultDB[elem]
         
-    world.plot(column='distribution', cmap='Greens', alpha=0.5, categorical=False, legend=False, ax=None)
+    world.plot(column='distribution', cmap='Greens', alpha=alphaValue, categorical=False, legend=False, ax=None)
 
     print(world)
     plt.show()
+
+def main():
+    world = GeoDataFrame.from_file('ne_10m_admin_0_countries.shp').sort_values(by='NAME').set_index('ISO_A3')
+    listDf = world.index.tolist()
+    
+    processMap(world, listDf, queryEJ1, 0.5)
+    processMap(world, listDf, queryEJ2, 0.5)
+    processMap(world, listDf, queryEJ3, 0.5)
+    
+    
 
 main()
