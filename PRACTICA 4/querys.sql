fe87edb221e83d82606891e73fcbf8a0d36ec347
MAURO MARTIN BONANNO <mauro.bonanno@mercadolibre.com>
2021-06-11 19:58:14 -0300

    arreglo del id

--- PRACTICA 4/querys.sql
+++ PRACTICA 4/querys.sql
@@ -1,12 +1,12 @@
 -- EJERCICIO 1
 -- Seleccionar los id y el texto de 10 documentos
-db.tweets.find({}, {text:1}).limit(10)
+db.tweets.find({}, {id:1, text:1}).limit(10)
 
 -- Seleccionar los lenguajes distintos de los tweets
 db.tweets.distinct("lang")
 
 -- Seleecionar el id, nombre, descripción y cantidad de followers de aquellos usuarios que tengan más de 100000 followers
-db.tweets.find({"user.followers_count": {$gt:100000}}, {"user.id":1,"user.name":1, "user.description":1, "user.followers_count":1})
+db.tweets.find({"user.followers_count": {$gt:100000}}, {id: 1, "user.id":1,"user.name":1, "user.description":1, "user.followers_count":1})
 
 -- Seleecionar el id, nombre y cantidad de followers de los 10 usuarios con más followers ordenado de manera descendente
 db.tweets.find({}, {"user.id":1,"user.name":1, "user.followers_count":1}).sort({"user.followers_count":-1}).limit(10)
@@ -14,7 +14,15 @@ db.tweets.find({}, {"user.id":1,"user.name":1, "user.followers_count":1}).sort({
 
 -- EJERCICIO 2
 -- En base al campo "source" determinar la cantidad de usuarios que hay por cada canal
+var map = function() { emit(this.source, 1);};
+var countFunction = function(key, values) {
+    var sum = 0;
+    for(var i in values) sum += values[i];
+    return sum;    
+}
 
+db.tweets.mapReduce(map, countFunction, { out: "total_por_source" });
+db.total_por_source.find();
 
 
 
@@ -32,6 +40,7 @@ db.total_por_lang.find();
 --Para validar: db.tweets.count({lang: "en"});
 
 
+-- Agregar validación para sacar los caracteres que no son alfa numericos
 -- Cantidad de tweets segun su longitud
 -- Corto < 10 palabras
 -- Mediano >= 10 -  < 20 palabras
